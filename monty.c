deb4ae4aec6e2378fbc98906eb7a3e7a21fdf882
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-01-19 03:30:29 +0200

    pop

--- monty.c
+++ monty.c
@@ -7,7 +7,7 @@
  *
  * Return: EXIT_FAILURE
  */
-void push(stack_t **stack, int value)
+void push(stack_t **stack, int value,unigned int line_number)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
@@ -47,4 +47,40 @@ void pall(stack_t *stack)
  * pint: prints the value of at the top of te stack, followed by newline
  * @stack: argument 1.
  *
- * Return : EXIT_FAILURE 
+ * Return : EXIT_FAILURE.
+ */
+void pint(stack_t *stack, unsigned int line_number)
+{
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L<line_number>: can't pint, stack empty\n",line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", stack->n);
+}
+
+/**
+ * pop: removes the top elemnt of the stack
+ * @stack: argument node.
+ *
+ * Return : EXIT_FAILURE on failue
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprint(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	stack_t *temp = *stack;
+	*stack =(*stack)->next;
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+
+	free(temp);
+}
--- monty.h
+++ monty.h
@@ -10,7 +10,9 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
-void push(stack_t **stack, int value);
+void push(stack_t **stack, int value,unsigned int line_number);
 void pall(stack_t *stack);
+void pint(stack_t *stack,unsigned int line_number);
+void pop(stack_t **stack),unsigned int line_number;
 
 #endif /* MONTY_h */
